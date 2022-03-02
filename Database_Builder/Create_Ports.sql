567a02d004f884cafe6daa213b2dd3b370d1e928
gmaynard1 <george.maynard@noaa.gov>
2022-03-02 14:23:50 -0500

    added region column

--- Database_Builder/Create_Ports.sql
+++ Database_Builder/Create_Ports.sql
@@ -9,6 +9,7 @@ CREATE TABLE `PORTS`
   `COUNTY_NAME` varchar(30) COMMENT 'Comes in from FVTR.FVTR_PORTS@sole',
   `LATITUDE` decimal(10,5) COMMENT 'Port latitude in decimal degrees. This number is manually generated for the purposes of eMOLT.',
   `LONGITUDE` decimal(10,5) COMMENT 'Port longitude in decimal degrees. This number is manually generated for the purposes of eMOLT.',
+  `EMOLT_REGION` set('New Jersey','Rhode Island','New Bedford','Cape Cod','North Shore','Maine','New York','Alaska','Canada') COMMENT 'Administrative regions of eMOLT (i.e., where support staff exist)',
 
   PRIMARY KEY (`PORT`)
-) COMMENT='This table is primarily read in from FVTR.FVTR_PORTS@sole, with the exception of lat/lon which much be manually added for each port.';
+) COMMENT='This table is primarily read in from FVTR.FVTR_PORTS@sole, with the exception of lat/lon and region which much be manually added for each port.';
