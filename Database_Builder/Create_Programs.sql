3e8546ba942f3608865e3d4fd59c31d0512b7413
gmaynard1 <george.maynard@noaa.gov>
2022-03-03 05:22:35 -0500

    foreign key syntax

--- Database_Builder/Create_Programs.sql
+++ Database_Builder/Create_Programs.sql
@@ -3,10 +3,12 @@ CREATE TABLE `PROGRAMS` (
   `PROGRAM_NAME` varchar(100) NOT NULL COMMENT 'The human-readable name of the program',
   `START_DATE` datetime NOT NULL COMMENT 'When the program began or is scheduled to begin',
   `END_DATE` datetime COMMENT 'When the program ended (leave NULL for ongoing)',
+  `LEAD_CONTACT` integer NOT NULL COMMENT 'References CONTACTS.CONTACT_ID',
   
-  PRIMARY KEY (`PROGRAM_ID`)
+  PRIMARY KEY (`PROGRAM_ID`),
   
-  KEY `fkIdx_100` (`LEAD_CONTACT`),
-  CONSTRAINT `FK_100` FOREIGN KEY `fkIdx_100` (`LEAD_CONTACT`) REFERENCES `CONTACTS` (`CONTACT_ID`),
+  CONSTRAINT fk_LeadContact 
+  FOREIGN KEY (`LEAD_CONTACT`) 
+    REFERENCES CONTACTS(CONTACT_ID)
   
 ) COMMENT='This table is used to explain which flavor(s) of eMOLT each vessel is associated with';
