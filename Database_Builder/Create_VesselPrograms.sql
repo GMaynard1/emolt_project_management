cb0917622578046da6051d155483f97852e73c19
George Maynard <george.maynard@noaa.gov>
2022-03-10 12:14:16 -0500

    Define primary key

--- Database_Builder/Create_VesselPrograms.sql
+++ Database_Builder/Create_VesselPrograms.sql
@@ -3,5 +3,8 @@ CREATE TABLE `VESSEL_PROGRAM` (
   `VESSEL_ID` integer NOT NULL COMMENT 'References VESSELS.VESSEL_ID',
   `PROGRAM_ID` integer NOT NULL COMMENT 'References PROGRAMS.PROGRAM_ID',
   `VP_START_DATE` datetime NOT NULL COMMENT 'The approximate date when the vessel joined a particular program',
-  `VP_END_DATE` datetime COMMENT 'The approximaate date when the vessel left a particular program (NULL for still active)'
+  `VP_END_DATE` datetime COMMENT 'The approximaate date when the vessel left a particular program (NULL for still active)',
+  
+  PRIMARY KEY(`VP_RECORD_ID`)
+  
 ) COMMENT='This table stores information tying vessels to specific programs. A single vessel may participate in multiple programs over time.';
