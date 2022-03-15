ca3878ed9669554708106d9669867037b1205156
George Maynard <george.maynard@noaa.gov>
2022-03-15 09:41:27 -0400

    Added missing constraints

--- Database_Builder/09_Create_VesselPrograms.sql
+++ Database_Builder/09_Create_VesselPrograms.sql
@@ -5,6 +5,12 @@ CREATE TABLE `VESSEL_PROGRAM` (
   `VP_START_DATE` datetime NOT NULL COMMENT 'The approximate date when the vessel joined a particular program',
   `VP_END_DATE` datetime COMMENT 'The approximaate date when the vessel left a particular program (NULL for still active)',
   
-  PRIMARY KEY(`VP_RECORD_ID`)
+  PRIMARY KEY(`VP_RECORD_ID`),
   
+  CONSTRAINT fk_vp_vid
+  FOREIGN KEY (`VESSEL_ID`) 
+    REFERENCES VESSELS(VESSEL_ID),
+  CONSTRAINT fk_vp_pid
+  FOREIGN KEY (`PROGRAM_ID`) 
+    REFERENCES PROGRAMS(PROGRAM_ID)
 ) COMMENT='This table stores information tying vessels to specific programs. A single vessel may participate in multiple programs over time.';
