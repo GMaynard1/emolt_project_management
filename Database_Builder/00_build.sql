d3250c4fd5bdbe9e2f5f3f005ba42098141a1874
George Maynard <george.maynard@noaa.gov>
2022-03-15 09:11:21 -0400

    finished, ready for testing

--- Database_Builder/00_build.sql
+++ Database_Builder/00_build.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE emolt_dev;
 USE emolt_dev;
 SOURCE /home/Database_Builder/01_Create_Ports.sql;
+SOURCE /home/Database_Builder/02_Create_Contacts.sql;
+SOURCE /home/Database_Builder/03_Create_Funding.sql;
+SOURCE /home/Database_Builder/04_Create_EquipmentInventory.sql;
+SOURCE /home/Database_Builder/05_Create_HardwareAddresses.sql;
+SOURCE /home/Database_Builder/06_Create_GearCodes.sql;
+SOURCE /home/Database_Builder/07_Create_Vessels.sql;
+SOURCE /home/Database_Builder/08_Create_Programs.sql;
+SOURCE /home/Database_Builder/09_Create_VesselPrograms.sql;
+SOURCE /home/Database_Builder/10_Create_VisitLog.sql;
+SOURCE /home/Database_Builder/11_Create_EquipmentChange.sql;
