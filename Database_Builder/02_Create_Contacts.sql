5a6e851f5974468783bc1a13e750266fad835d44
George Maynard <george.maynard@noaa.gov>
2022-03-16 14:49:16 -0400

    adds contact information for industry associations, research institutions, etc.

--- Database_Builder/02_Create_Contacts.sql
+++ Database_Builder/02_Create_Contacts.sql
@@ -1,3 +1,16 @@
+CREATE TABLE `PARTNER_GROUPS` (
+  `PARTNER_ID` integer NOT NULL AUTO_INCREMENT COMMENT 'A unique identifier used in this database only',
+  `PARTNER_NAME` varchar(100) NOT NULL COMMENT 'Organization name',
+  `PARTNER_STREET` varchar(50) COMMENT 'Partner street address',
+  `PARTNER_CITY` varchar(50) COMMENT 'City where mailing address is located',
+  `PARTNER_STATE` varchar(2) COMMENT 'Two character abbreviation of state or province used by postal service',
+  `PARTNER_ZIP` varchar(6) COMMENT 'Postal code, 5 characters for USA and 6 for Canada',
+  `PARTNER_PHONE` varchar(10) COMMENT 'Phone number in format xxxxxxxxxx',
+  `PARTNER_EMAIL` varchar(100) COMMENT 'Email address of partner organization',
+  
+  PRIMARY KEY(`PARTNER_ID`)
+) COMMENT='This table stores contact information for industry associations, fishing companies, research institutions, etc. that eMOLT participants are affiliated with.';
+
 CREATE TABLE `CONTACTS` (
   `CONTACT_ID` integer NOT NULL AUTO_INCREMENT COMMENT 'A unique identifier used in this database only',
   `FIRST_NAME` varchar(100) NOT NULL COMMENT 'Given name of contact',
@@ -16,3 +29,18 @@ CREATE TABLE `CONTACTS` (
   PRIMARY KEY (`CONTACT_ID`)
   
 ) COMMENT='This table stores contact information for eMOLT participants and support staff';
+
+CREATE TABLE `CONTACT_AFFILIATIONS`(
+  `AFFILIATION_ID`integer NOT NULL AUTO_INCREMENT COMMENT 'A unique identifier used in this database only',
+  `PARTNER_ID` integer NOT NULL COMMENT 'References PARTNER_GROUPS.PARTNER_ID',
+  `CONTACT_ID` integer NOT NULL COMMENT 'References CONTACTS.CONTACT_ID',
+  
+  PRIMARY KEY (`AFFILIATION_ID`),
+  
+  CONSTRAINT fk_pa_pid
+  FOREIGN KEY (`PARTNER_ID`) 
+    REFERENCES PARTNER_GROUPS(PARTNER_ID),
+  CONSTRAINT fk_pa_cid
+  FOREIGN KEY (`CONTACT_ID`) 
+    REFERENCES CONTACTS(CONTACT_ID)  
+) COMMENT='This table links eMOLT participants to industry associations, fishing companies, research institutions, etc.';
