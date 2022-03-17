675fd6bdc76b8ef8ee088fb69e49e4e14d68c9e0
George Maynard <george.maynard@noaa.gov>
2022-03-17 11:31:52 -0400

    scripts for loading independent tables to get this thing off the ground

---
+++ Database_Builder/DataLoading/load_PartnerGroups.sql
@@ -0,0 +1,6 @@
+LOAD DATA INFILE '/var/lib/mysql-files/Independent_Tables/Partners.csv'
+INTO TABLE PARTNER_GROUPS
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '/n'
+IGNORE 1 ROWS;
