f9fe4268d11f2b390c2f1b85c3bfd8a71076e5ba
George Maynard <george.maynard@noaa.gov>
2022-03-31 16:48:19 -0400

    change lowell user for remote access

--- Database_Builder/DeployUsers.sql
+++ Database_Builder/DeployUsers.sql
@@ -1,13 +1,13 @@
 CREATE USER 'jim'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT ON emolt_dev.* TO 'jim'@'localhost';
 
-CREATE USER 'lowell'@'localhost' IDENTIFIED BY 'password';
-GRANT SELECT ON emolt_dev.VESSELS TO 'lowell'@'localhost';
-GRANT SELECT ON emolt_dev.VESSEL_VISIT_LOG TO 'lowell'@'localhost';
-GRANT SELECT ON emolt_dev.EQUIPMENT_CHANGE TO 'lowell'@'localhost';
-GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'lowell'@'localhost';
-GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'lowell'@'localhost';
-GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'lowell'@'localhost'; 
+CREATE USER 'lowell'@'*' IDENTIFIED BY 'password';
+GRANT SELECT ON emolt_dev.VESSELS TO 'lowell'@'*';
+GRANT SELECT ON emolt_dev.VESSEL_VISIT_LOG TO 'lowell'@'*';
+GRANT SELECT ON emolt_dev.EQUIPMENT_CHANGE TO 'lowell'@'*';
+GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'lowell'@'*';
+GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'lowell'@'*';
+GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'lowell'@'*'; 
 
 CREATE USER 'vessel_status'@'*' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'vessel_status'@'*';
