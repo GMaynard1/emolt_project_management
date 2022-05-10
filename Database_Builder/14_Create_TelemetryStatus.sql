4c7043145a5f5b6febe03de89d25d46f46f4032e
George Maynard <george.maynard@noaa.gov>
2022-05-10 14:44:50 -0400

    added IP address field

--- Database_Builder/14_Create_TelemetryStatus.sql
+++ Database_Builder/14_Create_TelemetryStatus.sql
@@ -5,6 +5,7 @@ CREATE TABLE `TELEMETRY_STATUS`(
   `TS_REPORT_DATE` datetime NOT NULL COMMENT 'Timestamp of last telemetry status report',
   `TR_AMT_MB` decimal(10,2) COMMENT 'Amount of data transmitted since the last telemetry status report or in the last 24 hours, double check with Lowell',
   `HARDWARE_ID` integer NOT NULL COMMENT 'References eMOLT_dev.HARDWARE_ADDRESSES',
+  `IP_ADDRESS` varchar(16) COMMENT 'IP address of device',
   
   PRIMARY KEY (`TS_REPORT_ID`),
   
--- Database_Builder/DeployUsers.sql
+++ Database_Builder/DeployUsers.sql
@@ -1,15 +1,41 @@
 CREATE USER 'jim'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT ON emolt_dev.* TO 'jim'@'localhost';
 
-CREATE USER 'lowell'@'*' IDENTIFIED BY 'password';
-GRANT SELECT ON emolt_dev.VESSELS TO 'lowell'@'*';
-GRANT SELECT ON emolt_dev.VESSEL_VISIT_LOG TO 'lowell'@'*';
-GRANT SELECT ON emolt_dev.EQUIPMENT_CHANGE TO 'lowell'@'*';
-GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'lowell'@'*';
-GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'lowell'@'*';
-GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'lowell'@'*'; 
+CREATE USER 'lowell'@'%' IDENTIFIED BY 'password';
+CREATE USER 'lowell'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT ON emolt_dev.VESSEL_SIM TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.VESSEL_MAC TO 'lowell'@'%';
+GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'lowell'@'%'; 
+GRANT SELECT ON emolt_dev.VESSEL_SIM TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.VESSEL_MAC TO 'lowell'@'localhost';
+GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.EQUIPMENT_CHANGE TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.VESSELS TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.VESSEL_VISIT_LOG TO 'lowell'@'%';
+GRANT SELECT ON emolt_dev.EQUIPMENT_CHANGE TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.VESSELS TO 'lowell'@'localhost';
+GRANT SELECT ON emolt_dev.VESSEL_VISIT_LOG TO 'lowell'@'localhost';
 
 CREATE USER 'vessel_status'@'*' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'vessel_status'@'*';
 GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'vessel_status'@'*';
 GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'vessel_status'@'*';
+CREATE USER 'vessel_status'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'vessel_status'@'localhost';
+GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'vessel_status'@'localhost';
+GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'vessel_status'@'localhost';
+CREATE USER 'vessel_status'@'%' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT ON emolt_dev.TELEMETRY_STATUS TO 'vessel_status'@'%';
+GRANT SELECT ON emolt_dev.HARDWARE_ADDRESSES TO 'vessel_status'@'%';
+GRANT SELECT ON emolt_dev.EQUIPMENT_INVENTORY TO 'vessel_status'@'%';
+
+CREATE USER 'carles'@'%' IDENTIFIED BY 'password';
+CREATE USER 'carles'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT ON emolt_dev.VESSEL_MAC TO 'carles'@'%';
+GRANT SELECT ON emolt_dev.VESSEL_SIM TO 'carles'@'%';
+GRANT SELECT ON emolt_dev.VESSEL_MAC TO 'carles'@'localhost';
+GRANT SELECT ON emolt_dev.VESSEL_SIM TO 'carles'@'localhost';
