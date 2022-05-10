5fa74d44fa6c433e358de4e719f3b319c0136fa4
George Maynard <george.maynard@noaa.gov>
2022-05-10 16:10:22 -0400

    updated to include telemetry status function

--- UtilityScripts/API/plumber.R
+++ UtilityScripts/API/plumber.R
@@ -1,21 +1,18 @@
 # plumber.R
+require(plumber)
+source('login.R')
 
 #* Get MAC addresses associated with vessels
 #* @param vessel The vessel of interest
 #* @get /readMAC
 function(vessel="ALL"){
   ## Collect login info from file
-  user=read.csv("../../eMOLT-db/Login/logins.csv")$Username[1]
-  password=read.csv("../../eMOLT-db/Login/logins.csv")$Password[1]
-  dbname=read.csv("../../eMOLT-db/Login/logins.csv")$Database[1]
-  host=read.csv("../../eMOLT-db/Login/logins.csv")$Host[1]
-  port=read.csv("../../eMOLT-db/Login/logins.csv")$Port[1]
   ## Connect to database
   mydb=dbConnect(
     MySQL(), 
-    user=user, 
+    user=username, 
     password=password, 
-    dbname=dbname, 
+    dbname=db, 
     host=host,
     port=port
     )
@@ -34,18 +31,12 @@ function(vessel="ALL"){
 #* @param vessel The vessel of interest
 #* @get /readSIM
 function(vessel="ALL"){
-  ## Collect login info from file
-  user=read.csv("../../eMOLT-db/Login/logins.csv")$Username[1]
-  password=read.csv("../../eMOLT-db/Login/logins.csv")$Password[1]
-  dbname=read.csv("../../eMOLT-db/Login/logins.csv")$Database[1]
-  host=read.csv("../../eMOLT-db/Login/logins.csv")$Host[1]
-  port=read.csv("../../eMOLT-db/Login/logins.csv")$Port[1]
   ## Connect to database
   mydb=dbConnect(
     MySQL(), 
-    user=user, 
+    user=username, 
     password=password, 
-    dbname=dbname, 
+    dbname=db, 
     host=host,
     port=port
   )
@@ -60,45 +51,19 @@ function(vessel="ALL"){
   }
 }
 
-#* Add new vessel record to database
-#* @param vessel The vessel of interest
-#* @param main_contact Who is the best point of contact for the vessel
-#* @param owner Who owns the vessel
-#* @param captain Who operates the vessel
-#* @param tech who provides technical support for the vessel
-#* @param funding how was equipment purchase for the vessel funded
-#* @param home_port what is the vessel's homeport
-#* @param date when was the most recent visit to the vessel
-#* @param aquatec_sn  what is the serial number of the Aquatec unit on board
-#* @param lowell_sn what is the serial number of the lowell logger on board
-#* @param logger_change when was the logger changed most recently
-#* @param current_esn what is the satellite ESN of the vessel
-#* @param notes any additional notes about the vessel
-#* @param gear_type fixed or mobile
-#* @post /addVessel
-function(
-  vessel,
-  main_contact,
-  owner,
-  captain,
-  tech,
-  funding,
-  home_port,
-  date,
-  aquatec_sn="NULL",
-  lowell_sn="NULL",
-  logger_change="NULL",
-  current_esn="NULL",
-  notes="NULL",
-  gear_type
-  ){
-  ## Collect login info from file
-  user=read.csv("../../eMOLT-db/Login/logins.csv")$Username[2]
-  password=read.csv("../../eMOLT-db/Login/logins.csv")$Password[2]
-  dbname=read.csv("../../eMOLT-db/Login/logins.csv")$Database[2]
-  host=read.csv("../../eMOLT-db/Login/logins.csv")$Host[2]
-  port=read.csv("../../eMOLT-db/Login/logins.csv")$Port[2]
-  ## Connect to the database
+#* Insert a reboot record into the database
+#* @param timestamp The time that the device rebooted
+#* @param MAC The MAC address of the device
+#* @param IP The IP address of the device
+#* @post /insert_reboot
+function(timestamp, MAC, IP){
+  ## Read in the login credentials from a file
+  user=username
+  password=password
+  dbname=db
+  host=host
+  port=port
+  ## Connect to database
   mydb=dbConnect(
     MySQL(), 
     user=user, 
@@ -107,46 +72,41 @@ function(
     host=host,
     port=port
   )
-  ## Start a response message
-  response=paste0(
-    "Successfully connected to database at ",
-    Sys.time(),
-    ". "
-  )
-  ## Check to see if the funding source exists. If not, add a record.
-  x=dbGetQuery(
+  ## Look up the MAC address
+  temp=dbGetQuery(
     conn=mydb,
     statement=paste0(
-      "SELECT * FROM FUNDING WHERE FUNDING_AGENCY = '",
-      funding,
+      "SELECT * FROM VESSEL_MAC WHERE HARDWARE_ADDRESS = '",
+      MAC,
       "'"
     )
   )
-  if(nrow(x)==0){
-    dbGetQuery(
+  ## If the MAC address doesn't exist in the database, return an error
+  if(nrow(temp)==0){
+    stop("MAC address not found")
+  } else {
+    ## Otherwise, form an insert statement using the correct hardware ID
+    hid=dbGetQuery(
       conn=mydb,
       statement=paste0(
-        "INSERT INTO FUNDING (FUNDING_ID,FUNDING_AGENCY,START_DATE,END_DATE,FUNDING_AMOUNT_USD,PROPOSAL_LINK) VALUES (0,'",
-        funding,
-        "',NULL,NULL,0,NULL)"
+        "SELECT HARDWARE_ID FROM HARDWARE_ADDRESSES WHERE ADDRESS_TYPE = 'MAC' AND HARDWARE_ADDRESS = '",
+        MAC,
+        "'"
       )
+    )[1,1]
+    statement=paste0(
+      "INSERT INTO TELEMETRY_STATUS (`TS_REPORT_ID`,`TS_LATITUDE`,`TS_LONGITUDE`,`TS_REPORT_DATE`,`TR_AMT_MB`,`HARDWARE_ID`,`IP_ADDRESS`) VALUES (0,NULL,NULL,'",
+      timestamp,
+      "',NULL,",
+      hid,
+      ",'",
+      IP,
+      "')"
     )
-    response=paste0(
-      response,
-      "Added new record for ",
-      funding,
-      "to FUNDING. "
+    ## Execute the statement
+    dbGetQuery(
+      conn=mydb,
+      statement=statement
     )
   }
-  ## Store the appropriate funding ID for the new system
-  funding_id=dbGetQuery(
-    conn=mydb,
-    statement=paste0(
-      "SELECT FUNDING_ID FROM FUNDING WHERE FUNDING.FUNDING_AGENCY = '", 
-      funding,
-      "'"
-    )
-  )
-  
-  print(response)
 }
