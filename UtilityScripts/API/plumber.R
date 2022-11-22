ab8399f1bac16d1c97696fad5409e80e3aa8a7d3
George Maynard <george.maynard@noaa.gov>
2022-11-22 10:31:21 -0500

    updated API header to load QAQC R package

--- UtilityScripts/API/plumber.R
+++ UtilityScripts/API/plumber.R
@@ -1,8 +1,13 @@
 # plumber.R
+require(jsonlite)
 require(plumber)
 require(RMySQL)
 require(wkb)
 source('login.R')
+
+#* @apiTitle eMOLT dev API
+#* @apiDescription This is the development API for the eMOLT project.
+#* @apiContact list(name="API Support",email="george.maynard@noaa.gov") 
 
 
 #* Get MAC addresses associated with vessels
@@ -21,7 +26,7 @@ function(vessel="ALL"){
     )
   data=dbGetQuery(
     conn=mydb,
-    statement="SELECT * FROM VESSEL_MAC"
+    statement="SELECT * FROM vessel_mac"
   )
   if(vessel=="ALL"){
     print(data)
@@ -132,6 +137,16 @@ function(imei, device_type, serial, momsn, transmit_time, data, res){
       )
     )
   )
+  ## Use the IMEI number and the transmitter serial number to look up the vessel
+  ## in the database
+  mydb=dbConnect(
+    MySQL(), 
+    user=username, 
+    password=password, 
+    dbname=db, 
+    host=host,
+    port=port
+  )
   ## Parse the datastring into distinct elements
   ## Latitude
   raw=strsplit(datastring,",")[[1]][1]
@@ -140,26 +155,22 @@ function(imei, device_type, serial, momsn, transmit_time, data, res){
   raw=strsplit(datastring,",")[[1]][2]
   lon=as.numeric(substr(raw,1,2))*-1-as.numeric(substr(raw,3,nchar(raw)))/60
   ## Depth (m) 
-  ## CHECK WITH HUANXIN TO MAKE SURE THIS IS RIGHT
   raw=substr(strsplit(datastring,",")[[1]][3],1,3)
   depth=as.numeric(
-    paste0(
-      substr(raw,1,2),
-      ".",
-      substr(raw,3,3)
-    )
+    substr(raw,1,3)
   )
   ## Total range of depths observed (m)
-  ## CHECK WITH HUANXIN TO MAKE SURE THIS IS RIGHT
   raw=substr(strsplit(datastring,",")[[1]][3],4,6)
   rangedepth=as.numeric(
-    paste0(
-      substr(raw,1,2),
-      ".",
-      substr(raw,3,3)
-    )
+    substr(raw,1,3)
   )
   ## Soak time (minutes for mobile gear, hours for fixed gear)
+  ## The old way of recording soak time used different units depending on gear 
+  ## type. The new way will be a 5 character string of minutes
+  ##### NEW #####
+  #raw=as.numeric(substr(strsplit(datastring,",")[[1]][3],7,11))
+  #####
+  ##### OLD #####
   raw=as.numeric(substr(strsplit(datastring,",")[[1]][3],7,9))
   ## LOOK UP GEAR CODE ASSOCIATED WITH VESSEL HERE
   if(mobile==TRUE){
@@ -167,9 +178,11 @@ function(imei, device_type, serial, momsn, transmit_time, data, res){
   } else {
     soak=raw*60
   }
+  #####
   ## Average temperature (C)
-  raw=as.numeric(substr(strsplit(datastring,",")[[1]][3],10,14))/100
+  avgTemp=as.numeric(substr(strsplit(datastring,",")[[1]][3],10,13))/100
   ## Standard deviation of temperature (C)
+  stdTemp=as.numeric(substr(strsplit(datastring,",")[[1]][3],14,17))/100
   ## Last four characters of MAC Address and daily average temperatures 
   ## (up to five, fixed gear only)
   if(mobile!=TRUE){
@@ -205,7 +218,78 @@ function(imei, device_type, serial, momsn, transmit_time, data, res){
       1,
       4
     )
+    temps=NA
   }
   res$body=datastring
   res
+}
+
+#* Create the setup_rtd.py file that contains metadata for an emolt installation
+#* @param vessel The name of the vessel you are creating a setup file for
+#* @post /setup_rtd
+function(vessel){
+  vessel=toupper(gsub("_"," ",vessel))
+  time_range=1
+  Fathom=0.1
+  transmitter='yes'
+  mac_addr=dbGetQuery(
+    conn=conn,
+    statement=paste0(
+      "SELECT HARDWARE_ADDRESS FROM vessel_mac WHERE VESSEL_NAME = '",
+      toupper(gsub("_"," ",vessel)),
+      "'"
+    )
+  )$HARDWARE_ADDRESS
+  moana_SN=dbGetQuery(
+    conn=conn,
+    statement=paste0(
+      "SELECT SERIAL_NUMBER FROM vessel_mac WHERE VESSEL_NAME = '",
+      toupper(gsub("_"," ",vessel)),
+      "'"
+    )
+  )$SERIAL_NUMBER
+  gear_type=dbGetQuery(
+    conn=conn,
+    statement=paste0(
+      "SELECT GEAR_CODES.FMCODE FROM GEAR_CODES INNER JOIN VESSELS ON GEAR_CODES.GEAR_CODE = VESSELS.PRIMARY_GEAR WHERE VESSELS.VESSEL_NAME = '",
+      vessel,
+      "'"
+    )
+  )$FMCODE
+  if(gear_type=="F"){
+    gear_type="fixed"
+  } else {
+    if(gear_type=="M"){
+      gear_type="mobile"
+    } else {
+      if(gear_type=="O"){
+        gear_type="other"
+      } else {
+        gear_type="UNKNOWN: DATABASE UPDATE REQUIRED"
+      }
+    }
+  }
+  vessel_num=dbGetQuery(
+    conn=conn,
+    statement=paste0(
+      "SELECT EMOLT_NUM FROM VESSELS WHERE VESSEL_NAME = '",
+      vessel,
+      "'"
+    )
+  )$EMOLT_NUM
+  vessel_name=vessel
+  tilt="no"
+  path="/home/pi/rtd_global/"
+  sensor_type=dbGetQuery(
+    conn=conn,
+    statement=paste0(
+      "SELECT MAKE FROM vessel_mac WHERE VESSEL_NAME = '",
+      vessel,
+      "'"
+    )
+  )
+  time_diff_nke=0
+  tem_unit="Fahrenheit"
+  depth_unit="Fathoms"
+  local_time=-4
 }
