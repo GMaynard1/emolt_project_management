074dd6a4c56cad5df1593d4b9fd3f3494d2cf669
GMaynard1 <george.maynard@noaa.gov>
2025-09-24 11:12:07 -0400

    updated to clear action items temporary dataframe after adding to main dataframe

---
+++ VesselCheckIns/UMAINE/umaine_vessel_query.sql
@@ -0,0 +1,31 @@
+SELECT
+    boats.VESSEL_NAME,
+    primary_contacts.FIRST_NAME,
+    primary_contacts.LAST_NAME,
+    primary_contacts.PHONE,
+    primary_contacts.EMAIL,
+    boats.PRIMARY_FISHERY,
+    ports.PORT_NAME,
+    ports.STATE_POSTAL,
+    ports.LATITUDE,
+    ports.LONGITUDE,
+    gears.GEAR_CATEGORY
+FROM
+    VESSELS AS boats
+INNER JOIN
+    VESSEL_GEAR_TYPE AS gears ON boats.VESSEL_GEAR_TYPE_PK = gears.VESSEL_GEAR_TYPE_PK
+INNER JOIN
+    PORTS AS ports ON boats.PORT_PK = ports.PORT_PK
+INNER JOIN
+    CONTACTS AS contacts ON boats.TECHNICAL_CONTACT = contacts.CONTACT_PK
+INNER JOIN CONTACTS AS primary_contacts ON boats.PRIMARY_CONTACT = primary_contacts.CONTACT_PK
+WHERE
+    (
+		(contacts.LAST_NAME = 'GOODE' AND contacts.FIRST_NAME = 'ANDREW')
+		OR
+		(contacts.LAST_NAME = 'CASH' AND contacts.FIRST_NAME = 'CHRISTINA')
+	)
+    AND
+    (boats.ACTIVE=1)
+    ORDER BY VESSEL_NAME
+    ;
